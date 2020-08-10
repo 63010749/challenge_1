d7f5a431ffbb62a96dc4fb8a98d6386c4ca1bcf9
pawida sattayamuk <TangTana@hotmail.com>
2020-08-10 20:03:53 +0700

    comit1

--- challenge_1/challenge_1.c
+++ challenge_1/challenge_1.c
@@ -1,26 +1,41 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-	int i, j;
-	float average = 0, SD;
+	int i, j, n = 0, sum = 0, count = 0;
+	float average, sd2;
 	printf("Enter 2 numbers :\n");
 	scanf("%d", &i);
 	scanf("%d", &j);
-	
+
 	if (i > j) {
-		for (i = i; i >= j;i--) {
-			printf("%d",i);
-			average = average + i;
+		for (i = i; i >= j; i--) {
+			printf("%d ", i);
+			n = n + i;
+			sum = sum + i * i;
+			count++;
 		}
 		printf("\n");
 	}
+
 	else {
-		for (i = i; i <= j;i++) {
-
-			printf("%d", i);
+		for (i = i; i <= j; i++) {
+			printf("%d ", i);
+			n = n + i;
+			sum = sum + i * i;
+			count++;
 		}
 		printf("\n");
 	}
-	printf("%.1f", average);
 
+	average = (float)n / count;
+	printf("Average = %.1f\n", average);
+
+	sd2 = ((float)(count * sum) - (n * n)) / (count * (count - 1));
+
+	float sqrt1 = sqrt(sd2);
+
+	printf("S.D. = %.2f", sqrt1);
+
+	return 0;
 }
