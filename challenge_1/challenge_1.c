037acd7cdb0641d307e1d70e9a8cec9e7a573d4f
pawida sattayamuk <TangTana@hotmail.com>
2020-08-10 14:13:53 +0700

    Add project files.

---
+++ challenge_1/challenge_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+	int i, j;
+	float average = 0, SD;
+	printf("Enter 2 numbers :\n");
+	scanf("%d", &i);
+	scanf("%d", &j);
+	
+	if (i > j) {
+		for (i = i; i >= j;i--) {
+			printf("%d",i);
+			average = average + i;
+		}
+		printf("\n");
+	}
+	else {
+		for (i = i; i <= j;i++) {
+			printf("%d", i);
+		}
+		printf("\n");
+	}
+	printf("%.1f", average);
+}
